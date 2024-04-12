5c27d5fc167fe5fb3cf336d60b37c1e24ee44916
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-04-12 19:45:49 -0300

    Criar Migrations
    Começado a utilizar as Migrations para criar o banco de dados através delas ao invés de criar na mão.

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace ScreenSound.Banco;
 internal class ScreenSoundContext : DbContext
 {
-    private const string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    private const string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
---
+++ ScreenSound/Migrations/20240412223658_PopularTabela.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", ["Nome", "FotoPerfil", "Bio"],
+                ["Yuri", "yuri.png", "Um cara legal, doido."]);
+
+            migrationBuilder.InsertData("Artistas", ["Nome", "FotoPerfil", "Bio"],
+                ["Jems", "jems.png", "Um cara gostoso, doido."]);
+
+            migrationBuilder.InsertData("Artistas", ["Nome", "FotoPerfil", "Bio"],
+                ["Mateus", "mateus.png", "Um cara femboy, doido."]);
+
+            migrationBuilder.InsertData("Artistas", ["Nome", "FotoPerfil", "Bio"],
+                ["Luth", "luth.png", "Um cara arrombado, doido."]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Artistas");
+        }
+    }
+}
